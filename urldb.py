0e3d700c2f01d5dd8db664954e39956787f5c91c
nunref <nunref@gmail.com>
2025-04-09 22:54:11 +0200

    feat: add the database initialization as a flask command.

--- urldb.py
+++ urldb.py
@@ -1,25 +1,14 @@
 import sqlite3
 import datetime
+import click
 from flask import current_app, g
+
+DATABASE_NAME = "small_url.db"
 
 class URLDB:
     def __init__(self, database_name):
         self.database_name = database_name
         self.con = None
-
-        #TODO: move this from here.
-        self.connect()
-        CREATE_TABLE_SQL = """
-            CREATE TABLE IF NOT EXISTS url(
-                id INTEGER PRIMARY KEY,
-                original_url TEXT NOT NULL,
-                short_code TEXT NOT NULL,
-                date TEXT NOT NULL
-            )
-        """
-        cur = self.cursor()
-        cur.execute(CREATE_TABLE_SQL)
-        self.disconnect()
 
     def connect(self):
         print("connected")
@@ -69,7 +58,7 @@ class URLDB:
 
 def get_db():
     if "db" not in g:
-        g.db = URLDB("small_url.db")
+        g.db = URLDB(DATABASE_NAME)
         g.db.connect()
 
     return g.db
@@ -82,4 +71,28 @@ def close_db(e = None):
 
 def init_app(app):
     app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
 
+def init_db():
+    DROP_TABLE_SQL = "DROP TABLE IF EXISTS url";
+    CREATE_TABLE_SQL = """
+        CREATE TABLE IF NOT EXISTS url(
+            id INTEGER PRIMARY KEY,
+            original_url TEXT NOT NULL,
+            short_code TEXT NOT NULL,
+            date TEXT NOT NULL
+        )
+    """
+    urldb = URLDB(DATABASE_NAME)
+    urldb.connect()
+    cur = urldb.cursor()
+    cur.execute(DROP_TABLE_SQL)
+    cur.execute(CREATE_TABLE_SQL)
+    cur.close()
+    urldb.disconnect()
+
+@click.command("init-db")
+def init_db_command():
+    click.echo("Initializing the database ...")
+    init_db()
+    click.echo("Database initialized")
