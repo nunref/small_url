ca5b37bd6c19364880f28ef75162fdecd6f95b97
nunref <nunref@gmail.com>
2025-04-09 22:32:23 +0200

    ref: refactor the database to better handle connections.

--- app.py
+++ app.py
@@ -10,15 +10,22 @@ urldb = urldb.URLDB("small_url.db")
 def hello_world():
     if flask.request.method == "POST":
         random_code = generate_random_code()
+        urldb.connect()
         urldb.insert_url(flask.request.form["url_input"], random_code)
+        urldb.disconnect()
         return flask.render_template("code_show.html", code=random_code)
     else:
-        return flask.render_template("main.html", registered_urls = urldb.get_many_urls())
+        urldb.connect()
+        registered_urls = urldb.get_many_urls()
+        urldb.disconnect()
+        return flask.render_template("main.html", registered_urls = registered_urls)
  
 @app.route("/<code>")
 def redirect_to_code(code):
     print(code)
+    urldb.connect()
     original_url, short_code, date = urldb.get_url(code)
+    urldb.disconnect()
     return flask.redirect(original_url)
 
 def generate_random_code():
--- urldb.py
+++ urldb.py
@@ -4,7 +4,10 @@ import datetime
 class URLDB:
     def __init__(self, database_name):
         self.database_name = database_name
+        self.con = None
 
+        #TODO: move this from here.
+        self.connect()
         CREATE_TABLE_SQL = """
             CREATE TABLE IF NOT EXISTS url(
                 id INTEGER PRIMARY KEY,
@@ -13,44 +16,51 @@ class URLDB:
                 date TEXT NOT NULL
             )
         """
-        con = sqlite3.connect(self.database_name)
-        cur = con.cursor()
+        cur = self.cursor()
         cur.execute(CREATE_TABLE_SQL)
+        self.disconnect()
+
+    def connect(self):
+        self.con = sqlite3.connect(self.database_name)
+
+    def disconnect(self):
+        self.con.close()
+
+    def cursor(self):
+        return self.con.cursor()
+
+    def commit(self):
+        self.con.commit()
 
     def insert_url(self, original_url, short_code):
         SQL = "insert into url (original_url, short_code, date) values (?, ?, ?);"
         current_time = datetime.datetime.now()
         params = (original_url, short_code, current_time.isoformat())
 
-        con = sqlite3.connect(self.database_name)
-        cur = con.cursor()
+        cur = self.cursor()
         cur.execute(SQL, params)
-        con.commit()
-
         cur.close()
-        con.close()
+
+        self.commit()
 
     def get_url(self, short_code):
         SQL = "select original_url, short_code, date from url where short_code = ?;"
         params = (short_code,)
 
-        con = sqlite3.connect(self.database_name)
-        cur = con.cursor()
+        cur = self.cursor()
         res = cur.execute(SQL, params).fetchone()
-
         cur.close()
-        con.close()
+
+        self.close()
         return (res[0], res[1], datetime.datetime.fromisoformat(res[2]))
 
     def get_many_urls(self, urls_number=5):
         SQL = "select original_url, short_code from url order by date desc limit ?;"
         params = (urls_number,)
 
-        con = sqlite3.connect(self.database_name)
-        cur = con.cursor()
+        cur = self.cursor()
         res = cur.execute(SQL, params).fetchall()
-
         cur.close()
-        con.close()
+
         return res
 
